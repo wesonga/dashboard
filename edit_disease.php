23755840d2ec5e18c5452608f3b7196fe556e1a3
Wesonga Bob <31077716+wesonga@users.noreply.github.com>
2023-08-07 20:07:47 +0300

    Update edit_disease.php

--- edit_disease.php
+++ edit_disease.php
@@ -10,9 +10,10 @@ if (isset($_GET['id'])) {
         // Connect to your database (replace the placeholders with your actual database credentials)
         $servername = "localhost";
         $username = "root";
-        $password = "";
+        $password = "Password";
         $dbname = "ubuntu-kwanzaa";
-        $port = 3307; 
+        $port = 3306;
+
 
         $conn = new mysqli($servername, $username, $password, $dbname, $port);
         if ($conn->connect_error) {
